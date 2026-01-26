155d3fc7f2cb164157afcb4820310d1e0a58356a
Om Mittal <om040806mittal@gmail.com>
2026-01-26 05:43:47 +0530

    Script Update

--- Assets/Scripts/Parkours/High Beat/platformLifter.cs
+++ Assets/Scripts/Parkours/High Beat/platformLifter.cs
@@ -4,7 +4,6 @@ public class PlatformLifter : MonoBehaviour
 {
     [SerializeField] private LiftPlatform platformToLift;
 
-    // call this function when puzzle is complete
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
